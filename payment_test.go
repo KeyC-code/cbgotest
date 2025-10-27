4f36214319e1906cf33c627939b13875ba5881c7
KeyC-code <alexkov1956@gmail.com>
2025-10-27 20:45:36 +0300

    Пустой тест заготовка

---
+++ payment_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestPaymentValidation(t *testing.T) {
+
+	t.Run("Платеж ниже лимита - разрешаем", func(t *testing.T) {
+		t.Log("Платеж ниже лимита")
+	})
+}
